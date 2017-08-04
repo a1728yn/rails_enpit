ffc330878f7e640086a1a1c718675051463e3cae
KUROKI DAISHI <a1710dk@aiit.ac.jp>
2017-08-04 10:42:26 +0000

    fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,20 +6,22 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-require date
+#require "date"
 
 
 50.times do
-  from = Date.parse("2000/01/01")
-  to   = Date.parse("2000/12/31")
+#  from = Date.parse("2000/01/01")
+#  to   = Date.parse("2000/12/31")
 
   title = Faker::Book.title
   author = Faker::Book.author
   isbn = "1-234-5678-9123-4"
   status = "on_loan"
   summary = Faker::Lorem.sentence
-  published = Random.rand(from .. to)
-  modified = Random.rand(from .. to)
+#  published = Random.rand(from .. to)
+  published = "1990-08-09"
+#  modified = Random.rand(from .. to)
+  modified = "2017-08-09"
   
   Book.create(title: title, author: author, isbn: isbn, status: status, summary: summary, published: published, modified: modified)
 end 
